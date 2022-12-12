4e14fd2a66f204cd26a81ae0cacbcfde01d5bc4a
Fabio Luiz Alvares Osti <fabio.osti@hotmail.com>
2022-12-12 14:05:14 -0300

    Selected image now focus equivalent button

--- lib/views/conveyor_view.dart
+++ lib/views/conveyor_view.dart
@@ -1,53 +1,72 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:image_selector/controllers/selector_controller.dart';
-import 'package:image_selector/helper/listenable.dart';
 
-class ConveyorView extends StatefulWidget {
+class ConveyorView extends StatelessWidget {
   const ConveyorView({super.key});
 
   @override
-  State<ConveyorView> createState() => _ConveyorViewState();
+  Widget build(BuildContext context) {
+    return CustomScrollView(
+      slivers: [
+        SliverList(
+          delegate: SliverChildBuilderDelegate(
+            (context, index) {
+              File imgFile = SelectorController.imageFileQueue.get()[index];
+              return _ConveyorBox(imgFile: imgFile);
+            },
+            addAutomaticKeepAlives: false,
+            childCount: SelectorController.imageFileQueue.get().length,
+          ),
+        )
+      ],
+    );
+  }
 }
 
-class _ConveyorViewState extends State<ConveyorView> {
-  List<Widget>? generateList() {
-    return SelectorController.imageFileQueue.get().map((e) {
-      return Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: ElevatedButton(
-          style:
-              ElevatedButton.styleFrom(shape: const BeveledRectangleBorder()),
-          onPressed: () {
-            SelectorController.setSubject(e);
-          },
-          child: Image.file(e),
-        ),
-      );
-    }).toList();
-  }
+class _ConveyorBox extends StatefulWidget {
+  const _ConveyorBox({
+    Key? key,
+    required this.imgFile,
+  }) : super(key: key);
 
-  Unlisten? unlisten;
+  final File imgFile;
+
+  @override
+  State<_ConveyorBox> createState() => _ConveyorBoxState();
+}
+
+class _ConveyorBoxState extends State<_ConveyorBox> {
+  FocusNode? _focusNode;
 
   @override
   void initState() {
-    unlisten = SelectorController.listenQueueChanged(() {
-      setState(
-        () {},
-      );
-    });
+    _focusNode = FocusNode();
     super.initState();
   }
 
   @override
   void dispose() {
-    unlisten!();
+    _focusNode!.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(children: generateList() ?? []),
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ElevatedButton(
+        focusNode: _focusNode,
+        style: ElevatedButton.styleFrom(
+          shape: const BeveledRectangleBorder(),
+        ),
+        onPressed: () {
+          SelectorController.setSubject(widget.imgFile);
+          _focusNode!.requestFocus();
+        },
+        child: Image.file(widget.imgFile),
+      ),
     );
   }
 }
